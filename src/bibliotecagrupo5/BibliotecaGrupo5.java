7d6ec01b1bfdfc893cf4fdae222790e31582c48c
albertoFalum <alberto.alum@outlook.com.ar>
2023-10-03 16:20:01 -0300

    Initial commit

---
+++ src/bibliotecagrupo5/BibliotecaGrupo5.java
@@ -0,0 +1,17 @@
+
+package bibliotecagrupo5;
+
+/**
+ *
+ * @author BETO
+ */
+public class BibliotecaGrupo5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
