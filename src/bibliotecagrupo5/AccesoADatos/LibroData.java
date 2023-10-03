978a363aa20eeeb05fa5cbf78b93c00e6f607f38
albertoFalum <alberto.alum@outlook.com.ar>
2023-10-03 18:00:10 -0300

    Update LibroData

--- src/bibliotecagrupo5/AccesoADatos/LibroData.java
+++ src/bibliotecagrupo5/AccesoADatos/LibroData.java
@@ -1,6 +1,8 @@
 package bibliotecagrupo5.AccesoADatos;
 
+import bibliotecagrupo5.Entidades.Libro;
 import java.sql.Connection;
+import java.util.List;
 
 /**
  *
@@ -14,4 +16,36 @@ public class LibroData {
         con = Conexion.getConexion();
 
     }
+    
+    public void guardarLibro(Libro libro){
+        
+    }
+    
+    public void modificarLibro (Libro libro){
+    
+    }
+    
+    public void eliminarLibro (Libro libro){
+    
+    }
+    
+    public List <Libro> listarLibros(){
+    
+    }
+    
+    public Libro buscarLibro(int isbn){
+    
+    }
+    
+    public List<Libro> buscarLibrosPorAutor(String autor){
+    
+    }
+    
+    public Libro buscarLibroPorTitulo(String titulo){
+    
+    }
+    
+
+    
+    
 }
