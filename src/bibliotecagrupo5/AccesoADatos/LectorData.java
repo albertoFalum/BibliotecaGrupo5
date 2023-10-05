a1561fa8b38566565c5f89cf4e407debed81f4df
lean2110 <leandrocuello01@gmail.com>
2023-10-04 21:37:05 -0300

    a

---
+++ src/bibliotecagrupo5/AccesoADatos/LectorData.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bibliotecagrupo5.AccesoADatos;
+
+import bibliotecagrupo5.Entidades.Lector;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Leandro
+ */
+public class LectorData {
+      private Connection con = null;
+
+    public LectorData() {
+
+        con = Conexion.getConexion();
+
+    }
+     public void guardarLector(Lector lector) {
+        String sql = "INSERT INTO lector( nombre, apellido, domicilio, mail, estado)"
+                + "VALUES(?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+           
+            ps.setString(1, lector.getNombre());
+            ps.setString(2, lector.getApellido());
+            ps.setString(3, lector.getDomicilio());
+            ps.setString(4, lector.getMail());
+            ps.setBoolean(5, lector.isEstado());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+
+                lector.setNroSocio(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Lector Guardado");
+
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR LD1 - Error al acceder a la tabla lector: " + ex.getMessage());
+
+        }
+    }
+      public void modificarLector(Lector lector) {
+        String sql = "UPDATE lector SET nombre = ?, apellido = ?, domicilio = ?, mail = ? estado=? WHERE nroSocio = ?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, lector.getNombre());
+            ps.setString(2, lector.getApellido());
+            ps.setString(3, lector.getDomicilio());
+            ps.setString(4, lector.getMail());
+            ps.setBoolean(5, lector.isEstado());
+          
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Modificado Exitosamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "El lector no existe");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR LD2 - Error al acceder a la tabla Lector: " + ex.getMessage());
+        }
+    }
+}
--- src/bibliotecagrupo5/BibliotecaGrupo5.java
+++ src/bibliotecagrupo5/BibliotecaGrupo5.java
@@ -1,6 +1,8 @@
 package bibliotecagrupo5;
 
+import bibliotecagrupo5.AccesoADatos.LectorData;
 import bibliotecagrupo5.AccesoADatos.LibroData;
+import bibliotecagrupo5.Entidades.Lector;
 import bibliotecagrupo5.Entidades.Libro;
 import bibliotecagrupo5.Entidades.Tipo;
 
@@ -28,11 +30,19 @@ public class BibliotecaGrupo5 {
         
 //        Libro libro3=librodata.buscarLibroPorTitulo("santa evita");
 //        System.out.println(libro3);
+<<<<<<< Updated upstream
         
 //        librodata.eliminarLibro(1);
         
        Libro libro4= librodata.buscarLibroPorIsbn(422);
         System.out.println(libro4);
+=======
+//        
+//        librodata.eliminarLibro(1);
+        LectorData lectordata=new LectorData();
+        Lector lector=new Lector(1, "leandro", "cuello", "sudamerica1238", "leandro@gmail.com", true);
+        lectordata.modificarLector(lector);
+>>>>>>> Stashed changes
     }
    
 }
