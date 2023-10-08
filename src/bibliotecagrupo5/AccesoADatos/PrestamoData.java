4703237aa0da9ec2b0c2642f65df6acb6ad356d3
lean2110 <leandrocuello01@gmail.com>
2023-10-08 12:08:50 -0300

    a

--- src/bibliotecagrupo5/AccesoADatos/PrestamoData.java
+++ src/bibliotecagrupo5/AccesoADatos/PrestamoData.java
@@ -1,7 +1,16 @@
 
 package bibliotecagrupo5.AccesoADatos;
 
+import bibliotecagrupo5.Entidades.Prestamo;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -17,6 +26,74 @@ public class PrestamoData {
        ejemplardata=new EjemplarData();
        lectorData=new LectorData();
     }
-    
-    
+     public void guardarPrestamo(Prestamo  prestamo) {
+        String sql = "INSERT INTO prestamo(fechaInicio, fechaFin, codigo, nroSocio, estado)"
+                + "VALUES( ?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setDate(1, Date.valueOf(prestamo.getFechaInicio()));
+            ps.setDate(2,Date.valueOf(prestamo.getFechaFin()) );
+            ps.setInt(3, prestamo.getEjemplar().getCodigo());
+            ps.setInt(4, prestamo.getLector().getNroSocio());
+            ps.setBoolean(5, prestamo.isEstado());
+           
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+
+                prestamo.setIdPrestamo(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Prestamo Guardado");
+
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR P1 - Error al acceder a la tabla Libro: " + ex.getMessage());
+
+        }
+        
+    }
+      public void modificarEjemplar(Prestamo prestamo) {
+        String sql = "UPDATE prestamo SET fechaInicio = ?, fechaFin = ?, codigo = ?, nroSocio = ? WHERE idPrestamo = ?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setDate(1, Date.valueOf(prestamo.getFechaInicio()));
+            ps.setDate(2, Date.valueOf(prestamo.getFechaFin()));
+            ps.setInt(3, prestamo.getEjemplar().getCodigo());
+            ps.setInt(4, prestamo.getLector().getNroSocio());
+            ps.setInt(5, prestamo.getIdPrestamo());
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Modificado Exitosamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "El Prestamo no existe");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR P2 - Error al acceder a la tabla Prestamo: " + ex.getMessage());
+        }
+    }
+     public void eliminarPrestamo(int idPresta) {
+        String sql = "UPDATE prestamo SET estado = 0 WHERE idPrestamo = ?";
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idPresta);
+           
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+
+                JOptionPane.showMessageDialog(null, "Prestamo eliminado");
+
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR P3 - Error al acceder a la tabla Prestamo: " + ex.getMessage());
+        }
+    }
 }
--- src/bibliotecagrupo5/BibliotecaGrupo5.java
+++ src/bibliotecagrupo5/BibliotecaGrupo5.java
@@ -1,10 +1,17 @@
 package bibliotecagrupo5;
 
+import bibliotecagrupo5.AccesoADatos.EjemplarData;
 import bibliotecagrupo5.AccesoADatos.LectorData;
 import bibliotecagrupo5.AccesoADatos.LibroData;
+import bibliotecagrupo5.AccesoADatos.PrestamoData;
+import bibliotecagrupo5.Entidades.Condicion;
+import bibliotecagrupo5.Entidades.Ejemplar;
 import bibliotecagrupo5.Entidades.Lector;
 import bibliotecagrupo5.Entidades.Libro;
+import bibliotecagrupo5.Entidades.Prestamo;
 import bibliotecagrupo5.Entidades.Tipo;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.TreeSet;
 
 /**
@@ -24,15 +31,17 @@ public class BibliotecaGrupo5 {
 //        librodata.guardarLibro(libro);
 
 
-//        Libro libro= new Libro(1,422, "Santa Evita", "Tomas Eloy Martinez", 1975, Tipo.LITERATURA, "Sudamericana", true);
+        Libro libro= new Libro(1,422, "Santa Evita", "Tomas Eloy Martinez", 1975, Tipo.LITERATURA, "Sudamericana", true);
 //        librodata.modificarLibro(libro);
 //        Libro libro2=librodata.buscarLibro(1);
 //        System.out.println(libro2);
         
 //        Libro libro3=librodata.buscarLibroPorTitulo("santa evita");
 //        System.out.println(libro3);
-
-        
+            EjemplarData ejemplardata=new EjemplarData();
+            Ejemplar ejemplar=new Ejemplar(1,libro, 2, true, Condicion.DISPONIBLE);
+//            ejemplardata.guardarEjemplar(ejemplar);
+            
 //        librodata.eliminarLibro(1);
         
 //       Libro libro4= librodata.buscarLibroPorIsbn(422);
@@ -41,16 +50,23 @@ public class BibliotecaGrupo5 {
 //        
 //        librodata.eliminarLibro(1);
 //        LectorData lectordata=new LectorData();
-//       Lector lector=new Lector(1, "lucas", "cuello", "sudamerica1238", "leandro@gmail.com", true);
+       Lector lector=new Lector(1, "lucas", "cuello", "sudamerica1238", "leandro@gmail.com", true);
 //        lectordata.modificarLector(lector);
 //          lectordata.eliminarLector(1);
         
-        TreeSet<Libro> listaPorTipo=librodata.listarLibrosPorAutor("Gabriel Garcia Marquez");
+//        TreeSet<Libro> listaPorTipo=librodata.listarLibrosPorAutor("Gabriel Garcia Marquez");
+//        
+//        for(Libro aux:listaPorTipo){
+//            System.out.println(aux);
         
-        for(Libro aux:listaPorTipo){
-            System.out.println(aux);
-        
-        }
+//        }
+          PrestamoData prestamodata=new PrestamoData();
+          Prestamo prestamo=new Prestamo(3,LocalDate.of(1998, Month.MARCH, 24), LocalDate.of(1999, Month.MAY, 14), ejemplar, lector, true);
+//          prestamodata.guardarPrestamo(prestamo);
+//           prestamo.setFechaFin(LocalDate.of(1998, Month.MARCH, 5));
+//          prestamodata.modificarEjemplar(prestamo);
+            prestamodata.eliminarPrestamo(4);
+    
     }
    
 }
