39591b32f2513b7e44047b64d965e1c84b5605d1
lean2110 <leandrocuello01@gmail.com>
2023-10-25 17:32:01 -0300

    ok

--- src/bibliotecagrupo5/Vistas/MenuPrincipal.java
+++ src/bibliotecagrupo5/Vistas/MenuPrincipal.java
@@ -4,12 +4,9 @@
  */
 package bibliotecagrupo5.Vistas;
 
-
 import java.awt.Dimension;
 
-
 import javax.swing.JOptionPane;
-
 
 /**
  *
@@ -21,7 +18,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
      * Creates new form Menú
      */
     public MenuPrincipal() {
-        initComponents();    
+        initComponents();
     }
 
     /**
@@ -302,7 +299,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
         // TODO add your handling code here:
         jdpEscritorio.removeAll();
         jdpEscritorio.repaint();
-        ConsultaEjemplaresPrestadosPorFecha ceppf=new ConsultaEjemplaresPrestadosPorFecha();
+        ConsultaEjemplaresPrestadosPorFecha ceppf = new ConsultaEjemplaresPrestadosPorFecha();
         ceppf.setVisible(true);
         jdpEscritorio.add(ceppf);
 
@@ -311,7 +308,40 @@ public class MenuPrincipal extends javax.swing.JFrame {
         ceppf.setLocation((desktopSize.width - FrameSize.width) / 2, (desktopSize.height - FrameSize.height) / 2);
         ceppf.show();
     }//GEN-LAST:event_jMenuItem7ActionPerformed
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MenuPrincipal().setVisible(true);
+            }
+        });
+    }
     /**
      * @param args the command line arguments
      */
